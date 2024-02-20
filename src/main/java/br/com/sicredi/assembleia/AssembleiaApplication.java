78854b9f0a531044f92d5bedb0c7261bf81f144c
Rauan Santos <rauanmoreira259@gmail.com>
2024-02-20 11:43:46 -0300

    init

---
+++ src/main/java/br/com/sicredi/assembleia/AssembleiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaApplication.class, args);
+	}
+
+}
